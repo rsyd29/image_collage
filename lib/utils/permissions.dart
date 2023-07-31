fc144a4a148cbd388ba5c9bf3a1b05aecc623f2f
Budiman Rasyid <budiman.rasyid@legoas.co.id>
2023-07-31 11:54:03 +0700

    Fixing permissions on method _handleInvalidPermissions

--- lib/utils/permissions.dart
+++ lib/utils/permissions.dart
@@ -1,92 +1,135 @@
 import 'dart:io';
-import 'package:permission_handler/permission_handler.dart';
+
+import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class Permissions {
   ///Checking that camera and storage permission granted or not (Platform vise)
   static Future<bool> cameraAndStoragePermissionsGranted() async {
-    PermissionStatus cameraPermissionStatus = await _getCameraPermission();
-    switch (Platform.isAndroid ? 1 : 0) {
+    try {
+      PermissionStatus cameraPermissionStatus = await _getCameraPermission();
+      switch (Platform.isAndroid ? 1 : 0) {
+        ///For Android
+        case 1:
+          PermissionStatus storagePermissionStatus =
+              await _getStoragePermission();
 
-      ///For Android
-      case 1:
-        PermissionStatus storagePermissionStatus =
-            await _getStoragePermission();
+          if (cameraPermissionStatus == PermissionStatus.granted &&
+              storagePermissionStatus == PermissionStatus.granted) {
+            return true;
+          } else {
+            _handleInvalidPermissions(
+                cameraPermissionStatus, storagePermissionStatus);
+            return false;
+          }
 
-        if (cameraPermissionStatus == PermissionStatus.granted &&
-            storagePermissionStatus == PermissionStatus.granted) {
-          return true;
-        } else {
-          _handleInvalidPermissions(
-              cameraPermissionStatus, storagePermissionStatus);
+        ///For iOS
+        case 0:
+          if (cameraPermissionStatus == PermissionStatus.granted) {
+            return true;
+          } else {
+            _handleInvalidPermissions(cameraPermissionStatus, null);
+            return false;
+          }
+
+        default:
           return false;
-        }
-
-      ///For iOS
-      case 0:
-        if (cameraPermissionStatus == PermissionStatus.granted) {
-          return true;
-        } else {
-          _handleInvalidPermissions(cameraPermissionStatus, null);
-          return false;
-        }
-
-      default:
-        return false;
+      }
+    } catch (e, s) {
+      debugPrint('Exception cameraAndStoragePermissionsGranted: $e');
+      debugPrintStack(stackTrace: s);
+      rethrow;
     }
   }
 
   ///Checking camera permission
   static Future<PermissionStatus> _getCameraPermission() async {
-    PermissionStatus permission = await Permission.camera.status;
-    if (permission != PermissionStatus.granted) {
-      PermissionStatus permissionStatus = await Permission.camera.request();
-      return permissionStatus;
-    } else {
-      return permission;
+    try {
+      PermissionStatus permission = await Permission.camera.status;
+      if (permission != PermissionStatus.granted) {
+        PermissionStatus permissionStatus = await Permission.camera.request();
+        return permissionStatus;
+      } else {
+        return permission;
+      }
+    } catch (e, s) {
+      debugPrint('Exception _getCameraPermission: $e');
+      debugPrintStack(stackTrace: s);
+      rethrow;
     }
   }
 
   ///Checking storage permission
   static Future<PermissionStatus> _getStoragePermission() async {
-    PermissionStatus permission = await Permission.storage.status;
-    if (permission != PermissionStatus.granted) {
-      PermissionStatus permissionStatus = await Permission.storage.request();
-      return permissionStatus;
-    } else {
-      return permission;
+    try {
+      PermissionStatus permission = await Permission.storage.status;
+      if (permission != PermissionStatus.granted) {
+        PermissionStatus permissionStatus = await Permission.storage.request();
+        return permissionStatus;
+      } else {
+        return permission;
+      }
+    } catch (e, s) {
+      debugPrint('Exception _getStoragePermission: $e');
+      debugPrintStack(stackTrace: s);
+      rethrow;
     }
   }
 
-  ///Checking permission is availale or not (Platform specific)
-  static void _handleInvalidPermissions(PermissionStatus cameraPermissionStatus,
-      PermissionStatus? storagePermissionStatus) async {
-    if (Platform.isAndroid) _getStoragePermission();
-    PermissionStatus storagePermissionStatus = await _getStoragePermission();
+  ///Checking permission is available or not (Platform specific)
+  static void _handleInvalidPermissions(
+    PermissionStatus cameraPermissionStatus,
+    PermissionStatus? storagePermissionStatus,
+  ) async {
+    try {
+      if (Platform.isAndroid) {
+        if (storagePermissionStatus == null) {
+          PermissionStatus storagePermissionStatus =
+              await _getStoragePermission();
+          if (storagePermissionStatus.isPermanentlyDenied) {
+            await openAppSettings();
+            return;
+          }
+        } else {
+          if (storagePermissionStatus.isPermanentlyDenied) {
+            await openAppSettings();
+            return;
+          }
+        }
+      }
 
-    if (cameraPermissionStatus.isPermanentlyDenied) {
-      openAppSettings();
-      return;
+      if (cameraPermissionStatus.isPermanentlyDenied) {
+        await openAppSettings();
+        return;
+      }
+
+      ((cameraPermissionStatus == PermissionStatus.denied) &&
+              (storagePermissionStatus == PermissionStatus.denied))
+          ? permissionDeniedMessage()
+          : permissionDisabledMessage();
+    } catch (e, s) {
+      debugPrint('Exception _handleInvalidPermissions: $e');
+      debugPrintStack(stackTrace: s);
+      rethrow;
     }
-    ((cameraPermissionStatus == PermissionStatus.denied) &&
-            (storagePermissionStatus == PermissionStatus.denied))
-        ? permissionDeniedMessage()
-        : permissionDisabledMessage();
   }
 
   ///Throw message for permission denied
   static void permissionDeniedMessage() {
     throw PlatformException(
-        code: "PERMISSION_DENIED",
-        message: "Access to camera and storage denied",
-        details: null);
+      code: "PERMISSION_DENIED",
+      message: "Access to camera and storage denied",
+      details: null,
+    );
   }
 
   ///Throw message for permission disabled
   static void permissionDisabledMessage() {
     throw PlatformException(
-        code: "PERMISSION_DISABLED",
-        message: "Camera data is not available on device",
-        details: null);
+      code: "PERMISSION_DISABLED",
+      message: "Camera data is not available on device",
+      details: null,
+    );
   }
 }
